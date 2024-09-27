a78cf7c6d8f387d32e8eac19a0da3916f7321fba
pawani mekhala <pawanimekhala3@gmail.com>
2024-09-27 21:55:10 +0530

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // darkMode: 'media', // or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ text.js
@@ -0,0 +1,20 @@
+{/* upper navbar */}
+        {/* <div>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div className="container-fluid">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#">Appoinment</a></li>
+                        <li><a href="#">Staff</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div> */}
