e96eba0ac6ded9466b42dd4a2df750233644b1c5
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-15 00:24:01 +0530

    feat: add Home component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-// import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
       <Footer />
     </div>
   );
