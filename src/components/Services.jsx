7e8ade4857ce711aaca93950750215fc7caba798
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-18 00:34:05 +0530

    fix: replace class with className in Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,56 +8,38 @@ import Service6 from "../assets/services_img/service6.jpg";
 
 const Services = () => {
   const serviceItem = [
-    {
-      text: "Facial Treatments",
-      image: Service1,
-    },
-    {
-      text: "Standard Haircuts",
-      image: Service2,
-    },
-    {
-      text: "Hair Treatments",
-      image: Service3,
-    },
-    {
-      text: "Styling",
-      image: Service4,
-    },
-    {
-      text: "Body Treatments",
-      image: Service5,
-    },
-    {
-      text: "Weddings",
-      image: Service6,
-    },
+    { text: "Facial Treatments", image: Service1 },
+    { text: "Standard Haircuts", image: Service2 },
+    { text: "Hair Treatments", image: Service3 },
+    { text: "Styling", image: Service4 },
+    { text: "Body Treatments", image: Service5 },
+    { text: "Weddings", image: Service6 },
   ];
 
   return (
-    <div class="py-24 ">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="py-24 ">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {/* Services heading */}
-        <h2 class="text-7xl font-semibold text-yellow-700 text-center mb-14">
+        <h2 className="text-7xl font-semibold text-yellow-700 text-center mb-14">
           Our Services...
         </h2>
-        <div class="mx-auto mt-8 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
+        <div className="mx-auto mt-8 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
           {/* Services */}
           {serviceItem.map(({ text, image }, index) => {
             return (
               <div
                 key={index}
-                class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+                className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
               >
                 <img
                   src={image}
                   alt=""
-                  class="absolute inset-0 -z-10 h-full w-full object-cover"
+                  className="absolute inset-0 -z-10 h-full w-full object-cover"
                 />
-                <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
-                <h4 class="mt-3 text-lg font-semibold leading-6 text-white">
-                  <span class="absolute inset-0"></span>
+                <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+                <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                <h4 className="mt-3 text-lg font-semibold leading-6 text-white">
+                  <span className="absolute inset-0"></span>
                   {text}
                 </h4>
               </div>
