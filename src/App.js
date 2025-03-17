389187448ca821090cbd5d5cd001984869e8abc3
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-18 00:26:23 +0530

    feat: add Services component to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import OurTeam from "./components/OurTeam";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <AboutUs/>
       <OurTeam/>
+      <Services/>
       <Footer />
     </div>
   );
