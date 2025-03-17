b468934268da374a6e38304ef57e45490eee5d35
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-18 00:35:30 +0530

    fix: replace class with className in OurTeam component

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -7,88 +7,88 @@ import Owner from "../assets/images for saloon/owner.jpg";
 
 const OurTeam = () => {
   return (
-    <div class="py-24 ">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="py-24 ">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {/* Main Text  */}
-        <div class="mb-12">
-          <h2 class="text-7xl text-center font-semibold text-yellow-700 ">
+        <div className="mb-12">
+          <h2 className="text-7xl text-center font-semibold text-yellow-700 ">
             Our team...{" "}
           </h2>
         </div>
         {/* Staff Image Gallery */}
-        <div class="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-6 lg:grid-cols-5 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-full">
-          <div class="block group md:col-span-2 lg:col-span-1 ">
-            <div class="relative mb-6">
+        <div className="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-6 lg:grid-cols-5 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-full">
+          <div className="block group md:col-span-2 lg:col-span-1 ">
+            <div className="relative mb-6">
               <img
                 src={Owner}
                 alt="Antonio"
-                class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
+                className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
               />
             </div>
-            <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+            <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
               T.B.Perera{" "}
             </h4>
-            <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+            <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
               Founder
             </span>
           </div>
-          <div class="block group md:col-span-2 lg:col-span-1 ">
-            <div class="relative mb-6">
+          <div className="block group md:col-span-2 lg:col-span-1 ">
+            <div className="relative mb-6">
               <img
                 src={Staff2}
                 alt="Patricia"
-                class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
+                className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
               />
             </div>
-            <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+            <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
               Patricia Angely{" "}
             </h4>
-            <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+            <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
               Co-Founder
             </span>
           </div>
-          <div class="group group md:col-span-2 lg:col-span-1">
-            <div class="relative mb-6">
+          <div className="group group md:col-span-2 lg:col-span-1">
+            <div className="relative mb-6">
               <img
                 src={Staff3}
                 alt="Jerom"
-                class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
+                className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
               />
             </div>
-            <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+            <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
               Jerom Bell{" "}
             </h4>
-            <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+            <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
               Chairman
             </span>
           </div>
-          <div class="block group md:col-span-2 lg:col-span-1 md:col-start-2 lg:col-start-4">
-            <div class="relative mb-6">
+          <div className="block group md:col-span-2 lg:col-span-1 md:col-start-2 lg:col-start-4">
+            <div className="relative mb-6">
               <img
                 src={Staff4}
                 alt="Yasmine"
-                class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
+                className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
               />
             </div>
-            <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+            <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
               Yasmine Tano{" "}
             </h4>
-            <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+            <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
               CEO
             </span>
           </div>
-          <div class="block group min-[500px]:col-span-2 mx-auto md:col-span-2 lg:col-span-1 ">
-            <div class="relative mb-6">
+          <div className="block group min-[500px]:col-span-2 mx-auto md:col-span-2 lg:col-span-1 ">
+            <div className="relative mb-6">
               <img
                 src={Staff1}
                 alt="Martin"
-                class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
+                className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
               />
             </div>
-            <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+            <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
               Martin Darbys
             </h4>
-            <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+            <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
               Product Manager
             </span>
           </div>
