dbcdfcc7a5624b43b3223dec0f19b92fd218c087
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-17 22:42:08 +0530

    feat: add AboutUs component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+
+
 
 
 function App() {
@@ -8,6 +11,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <AboutUs/>
       <Footer />
     </div>
   );
