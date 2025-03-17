3df04cadeefc4b9558db46439dcb1f3a54997a73
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-17 22:45:58 +0530

    feat: add heading text

---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <div class="py-24 ">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="mb-12">
+          <h2 class="text-7xl text-center font-semibold text-yellow-700 ">
+            Our team{" "}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurTeam;
