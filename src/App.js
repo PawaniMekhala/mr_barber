d64d828109c708e3a2324bf870f24162b5b8dfc6
PawaniMekhala <pawanimekhala3@gmail.com>
2025-03-18 23:36:52 +0530

    feat: add gallery component to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import OurTeam from "./components/OurTeam";
 import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <AboutUs/>
       <OurTeam/>
       <Services/>
+      <Gallery/>
       <Footer />
     </div>
   );
