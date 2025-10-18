50a489e1c292512c92678e6d023d6d1c425bf7e9
PawaniMekhala <pawanimekhala3@gmail.com>
2025-10-19 01:02:19 +0530

    chore: extend Tailwind config with Playfair font familiy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   // darkMode: 'media', // or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
