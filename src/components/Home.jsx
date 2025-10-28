74bfc37b5a0003c3a5e8e249f6b61d0d7746197e
PawaniMekhala <pawanimekhala3@gmail.com>
2025-10-28 21:56:24 +0530

    refactor: change hours + contact box text color to white

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -143,13 +143,13 @@ const Home = () => {
           {/* Contact + Hours Box */}
           <div className="flex flex-col sm:flex-row justify-between items-center border border-yellow-700 rounded-lg px-6 py-4 gap-4 text-center sm:text-left">
             {/* Call */}
-            <div className="flex items-center gap-2 text-yellow-700 text-lg font-poppins font-medium">
+            <div className="flex items-center gap-2 text-white text-lg font-poppins font-medium">
               <i className="fa-brands fa-whatsapp text-xl"></i>
               <span>Call Us : +94 782637487</span>
             </div>
 
             {/* Opening Hours */}
-            <div className="flex items-center gap-2 text-yellow-700 text-lg font-poppins font-medium">
+            <div className="flex items-center gap-2 text-white text-lg font-poppins font-medium">
               <i className="fa-regular fa-calendar-days text-xl"></i>
               <span>Opening Hours : Sunday – Friday, 08 a.m – 09 p.m</span>
             </div>
