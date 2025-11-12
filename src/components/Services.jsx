b7e40b240003409c24ec46d8df02ceca3a2cdab2
PawaniMekhala <pawanimekhala3@gmail.com>
2025-11-12 12:09:47 +0530

    feat: add transition effect to displayservices card text

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -24,23 +24,22 @@ const Services = () => {
         <h2 className="text-7xl font-semibold font-playfair text-yellow-700 mb-14">
           Our Services...
         </h2>
+        {/* Services cards */}
         <div className="mx-auto mt-8 grid font-poppins max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
-          {/* Services */}
           {serviceItem.map(({ text, image }, index) => {
             return (
               <div
                 key={index}
-                className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+                className="group relative isolate flex flex-col justify-center items-center overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
               >
                 <img
                   src={image}
-                  alt=""
+                  alt={text}
                   className="absolute inset-0 -z-10 h-full w-full object-cover"
                 />
                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
                 <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
-                <h4 className="mt-3 text-lg font-semibold leading-6 text-white">
-                  <span className="absolute inset-0"></span>
+                <h4 className="text-2xl font-medium text-white text-center transition-transform duration-300 ease-in-out hover:-translate-y-2 group-hover:text-yellow-700">
                   {text}
                 </h4>
               </div>
