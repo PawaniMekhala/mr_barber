8dc832102d047279e652ca73779de37e471c67db
PawaniMekhala <pawanimekhala3@gmail.com>
2025-11-14 23:10:57 +0530

    feat: install react icons dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   // darkMode: 'media', // or 'class'
   theme: {
     extend: {
+      colors: {
+        gold: {
+          50:  '#fffaf0',
+          100: '#fff0d9',
+          200: '#ffe0a8',
+          300: '#ffd078',
+          400: '#ffc048',
+          500: '#d39a08', // main gold, tweak as needed
+          600: '#b07a05',
+          700: '#876806',
+        }},
       fontFamily: {
         playfair: ['"Playfair Display"', 'serif'],
          poppins: ['Poppins', 'sans-serif'],
