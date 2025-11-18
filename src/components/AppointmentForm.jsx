b6651f31abc981b80d83ba4acfe404273494724d
PawaniMekhala <pawanimekhala3@gmail.com>
2025-11-18 23:55:53 +0530

    refactor: add necessary commits

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -41,7 +41,7 @@ const AppointmentForm = () => {
         </h1>
 
         <form className="space-y-8">
-          {/* top row (name) */}
+          {/* name, email and phone section */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4">
             <div className="md:col-span-2">
               <Input label="Name" name="name" type="text" />
