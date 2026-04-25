fcfbc7c15d3fa70244b0c70923117104dc964b71
PawaniMekhala <pawanimekhala3@gmail.com>
2026-04-25 21:59:52 +0530

    remove unnecessary comments

--- src/pages/StaffDetails.jsx
+++ src/pages/StaffDetails.jsx
@@ -1,20 +1,3 @@
-// import { useParams } from "react-router-dom";
-
-// export default function StaffDetails() {
-//   const { id } = useParams();
-
-//   return (
-//     <div className="min-h-screen bg-black text-white p-10">
-//       <h1 className="text-3xl text-gold-700">Staff Details - ID: {id}</h1>
-
-//       <p className="mt-4">
-//         Here you will show appointments for this staff member.
-//       </p>
-//     </div>
-//   );
-// }
-
-// pages/Roshan.jsx
 import { CheckCircle, XCircle, MessageSquare } from "lucide-react";
 
 const appointments = Array(6).fill({
