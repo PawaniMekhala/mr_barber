b7cbf4c758d9e67ecd27cfe21bb0333690bf4ceb
PawaniMekhala <pawanimekhala3@gmail.com>
2026-04-30 18:01:55 +0530

    fix: remove unused index

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -26,7 +26,7 @@ const Services = () => {
         </h2>
         {/* Service cards */}
         <div className="mx-auto mt-8 grid font-poppins max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
-          {services.map(({ id, text, image }, index) => {
+          {services.map(({ id, text, image }) => {
             return (
               <div
                 key={id}
