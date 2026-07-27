49ab6b90b8dac708d60fb108acb90bad32c3dd4f
PawaniMekhala <pawanimekhala3@gmail.com>
2026-07-27 08:26:07 +0530

    feat: add frontend files to frontend folder

---
+++ frontend/text.js
@@ -0,0 +1,27 @@
+// services.jsx
+// line: 29
+// fix: remove unused index 
+// line: 42
+// fix: fix hover effect issue
+// line:33
+// fix: fixed height using padding trick
+// line: 9
+// fix: Naming convention
+
+
+// here is the thing, i have a project in react, i will 
+// give package.json file, i need to know how to design 
+// a database, for this project, i will explain the 
+// functioons, there are separate sections, home, 
+// services,pricing,reviews,and etc, services has some 
+// categories like, hair cutting,styling, colouring and 
+// etc, each thing in the categories have prices, users 
+// can log in to the system and they can make appoinmrnt 
+// by filling a form which  has fields like, name, email,
+// phone no, select category, date, time and select 
+// barber, and message after input these details they can
+// make appoinmet, staff can also loginto the sysytem and 
+// they can see their profiles like customers, in staff 
+// details needs to have name, position, descripton, and 
+// every staff has his appoinment list , users can add 
+// reviews, with name,rating and message 
--- text.js
+++
@@ -1,9 +0,0 @@
-// services.jsx
-// line: 29
-// fix: remove unused index 
-// line: 42
-// fix: fix hover effect issue
-// line:33
-// fix: fixed height using padding trick
-// line: 9
-// fix: Naming convention
